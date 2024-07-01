f3fd4a1fe24e3750974f9514b32094514bddf8b7
Poojan Patel <ppatel2099@gmail.com>
2024-06-30 21:03:49 -0700

    first commit

---
+++ js/contactPost.js
@@ -0,0 +1,19 @@
+$('#contactForm').on('submit', function(event) {
+    event.preventDefault(); // prevent reload
+    
+    var formData = new FormData(this);
+    formData.append('service_id', 'service_p63zgac');
+    formData.append('template_id', 'template_1varwco');
+    formData.append('user_id', 'ppatel2099');
+ 
+    $.ajax('https://api.emailjs.com/api/v1.0/email/send-form', {
+        type: 'POST',
+        data: formData,
+        contentType: false, // auto-detection
+        processData: false // no need to parse formData to string
+    }).done(function() {
+        alert('Your mail is sent!');
+    }).fail(function(error) {
+        alert('Oops... ' + JSON.stringify(error));
+    });
+});
